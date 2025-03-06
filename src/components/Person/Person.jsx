ef0741c12d17295b05c9b5cf16b4cb4686e9f09f
rvemuri2 <rohitvemuri@yahoo.com>
2025-03-05 18:41:57 -0800

    avoided using ternary

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,25 @@
 // export const Person = ({ person }) => ();
 
+const getPartnerText = (person) => {
+    if (person.isMarried) {
+      if (person.gender === "m") {
+        return `${person.partnerName} is my wife`;
+      } else if (person.gender === "f") {
+        return `${person.partnerName} is my husband`;
+      }
+    }
+    return "I am not married";
+  };
+
 export const Person = ({person}) => {
+
     return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age && (
         <p className="Person__age">I am {person.age}</p>
       )}
-     <p className="Person__partner">
-    {person.isMarried
-      ? person.gender === "m"
-        ? `${person.partnerName} is my wife`
-        : `${person.partnerName} is my husband`
-      : "I am not married"}
-  </p>
+     <p className="Person__partner">{getPartnerText(person)}</p>
     </section>
     )
 }
